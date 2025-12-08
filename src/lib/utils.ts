c97ad84ef547bce6806c0b0e85e6072679ab9c23
Wesley Bliss <wesley.bliss@gmail.com>
2025-12-08 19:17:52 +0100

    Added sha256 library & hash method stub

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { sha256 } from "js-sha256"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export const hashFile = async (file: File): Promise<string> => {
+    const arrayBuffer = await file.arrayBuffer()
+    return sha256(arrayBuffer)
 }
